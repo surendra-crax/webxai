7ff3e034e6c1684565226611682aa4290441ea01
Bandaru Surendra <118333138+surendra-crax@users.noreply.github.com>
2025-02-10 18:35:32 +0530

    Update hero.jsx

--- components/hero.jsx
+++ components/hero.jsx
@@ -31,9 +31,9 @@ const HeroSection = () => {
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
           <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
-            Your AI Career Coach for
+            Your Personal AI Mentor for 
             <br />
-            Professional Success
+            Career Advancement
           </h1>
           <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
             Advance your career with personalized guidance, interview prep, and
